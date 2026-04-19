6479280c109dfa567d8cf4ad614055f3461fcf50
Ana Paula Maroubo <anapaula.maroubo@gmail.com>
2026-04-19 10:51:57 -0300

    fix: ajustar configuracao e corrigir execucao dos testes na pipeline

--- etapa-2/tests/front-end/utils/auth.js
+++ etapa-2/tests/front-end/utils/auth.js
@@ -3,6 +3,6 @@ import { LoginPage } from '../pages/LoginPage';
 export async function login(page, username = 'standard_user', password = 'secret_sauce') {
   const loginPage = new LoginPage(page);
 
-  await loginPage.goTo();
+  await loginPage.goto();
   await loginPage.login(username, password);
 }
--- playwright.config.js
+++ playwright.config.js
@@ -4,11 +4,12 @@ const isCI = !!process.env.CI;
 
 export default defineConfig({
   use: {
-    channel: 'chrome',
     headless: true,
+    ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
     baseURL: 'https://brasilapi.com.br',
+    ...(isCI ? {} : { channel: 'chrome' }),
   },
 });
